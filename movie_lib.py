7569e9c2714df0783d45a5949b8ae1c581c8f343
Matt Joyce <mattjoyce@Matts-MBP.PK5001Z>
2016-11-06 22:12:34 -0500

    Refining, still...

--- movie_lib.py
+++ movie_lib.py
@@ -21,15 +21,15 @@ class Movies:
 
     with open('ml-100k/u.item', encoding='latin_1') as i:
         reader_i = csv.DictReader(i)
-        # for row in reader_i:
-        #     print(row)
+         for row in reader_i:
+             print(row)
 
 
 
-    # with open('ml-100k/u.genre', encoding='latin_1') as g:
-    #     reader_g = csv.DictReader(g)
-    #     for row in reader_g:
-    #         print(row)
+    with open('ml-100k/u.genre', encoding='latin_1') as g:
+        reader_g = csv.DictReader(g)
+        for row in reader_g:
+            print(row)
 
 
 
@@ -42,52 +42,52 @@ class Movies:
         print(row_split)
 
 
-        # def columize(Header):
-        #     header = next(self.reader_g)
-        #     print(header)
+        def columize(Header):
+            header = next(self.reader_g)
+            print(header)
 
 
-    # def movie_info():
-    #
-    #     pass
+    def movie_info():
+
+        pass
 
 
 
-# class Users:
-#
-#     with open('ml-100k/u.user', encoding='latin_1') as u:
-#         reader = csv.DictReader(u)
-#         print(reader)
-#         for row in reader:
-#             print(row)
-#
-#     def __init__(self):
-#         pass
-#
-#     def user_info():
-#
-#         pass
-#
-# class Rating:
-#
-#     with open('ml-100k/u.data', encoding='latin_1') as d:
-#         reader = csv.DictReader(d)
-#         print(reader)
-#         for row in reader:
-#              print(row)
-#
-#
-#     def __init__(self):
-#
-#         pass
-#
-#     def rating_info():
-#
-#         pass
+class Users:
 
-# def get_avg(**kwargs):
-#     kwargs["open"] + float(kwargs["close"]) + float(kwargs["high"]) + float(kwargs["low"])
-#
-# def avg(self):
-#     1 = (self.open,self.close,self.low,self.high)
-#     return sum(1)/len(1)
+    with open('ml-100k/u.user', encoding='latin_1') as u:
+        reader = csv.DictReader(u)
+        print(reader)
+        for row in reader:
+            print(row)
+
+    def __init__(self):
+        pass
+
+    def user_info():
+
+        pass
+
+class Rating:
+
+    with open('ml-100k/u.data', encoding='latin_1') as d:
+        reader = csv.DictReader(d)
+        print(reader)
+        for row in reader:
+             print(row)
+
+
+    def __init__(self):
+
+        pass
+
+    def rating_info():
+
+        pass
+
+def get_avg(**kwargs):
+    kwargs["open"] + float(kwargs["close"]) + float(kwargs["high"]) + float(kwargs["low"])
+
+def avg(self):
+    1 = (self.open,self.close,self.low,self.high)
+    return sum(1)/len(1)
